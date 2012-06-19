14e2e95b5f5e4b9a0c3f0e7eb7a438163a6130c2
Clinton Bowen <clinton.bowen@gmail.com>
2012-06-18 19:40:20 -0700

    Updates with methods for comments

--- Pyllumina.py
+++ Pyllumina.py
@@ -3,17 +3,41 @@ from MetaSimPrimaryConfig import MetaSimPrimaryConfig
 from Metasimian import Metasimian
 from FastaSequence import *
 class Pyllumina:
+    """
+    This class is designed to be the top-level class for all code.  The user should use only this class.
+    """
     def FastaFileInputChecker(self,Input):
-        if len(Input) > 1:
-            #break it up
-    def __init__(self,Fasta_FileName,ExpCov,MinCutoff,ErrorModel,PairLength):
-        self.FastaFileInput = fasta_read(Fasta_FileName)
-        self.FastaFileInputChecker(self.FastaFileInput)#Make sure this checks how many sequences we have; if more than one break it up into several files for metasim to read
+        """
+        If Input is an array data structure, it returns 0, if Input is a string type, it returns 1
+        """
+        if type(Input) == list or type(Input) == tuple:
+            return 0
+        else:
+            return 1 
+    def SetSequenceLength(self,FSequence):
+        """
+        Input: FastaSequence object or FastaSequence array
+        Output: Sequence Length int or int array
+        """
+        if type(Input) == list or type(Input) == tuple:
+            self.SequenceLength = [len(i.seq) for i in FSequence]
+        else:
+            self.SequenceLength = len(FSequence.seq)
+    def __init__(self,Fasta_FileName,ExpCov,KMER,ErrorModel):
+        if self.FastaFileInputChecker(Fasta_FileName)==1:#Make sure this checks how many sequences we have; if more than one break it up into several files for metasim to read
+            self.FastaSequence = fasta_read(Fasta_FileName)
+        else:
+            self.FastaSequence = [fasta_read(i) for i in Fasta_FileName]
+        self.SetSequenceLength(self.FastaSequence)
         self.FastaFileName = Fasta_FileName
+        self.ExpectedCoverage = ExpCov
+        self.KmerLength = KMER
         self.MetaSimPrimary = MetaSimPrimaryConfig()
         self.MetaSimPrimary.Model = ErrorModel
         self.MetaSimSimulator = MetaSimSimulatorConfig()
         self.MetaSym = Metasimian(self.MetaSimPrimary,self.MetaSimSimulator)
+        self.MetaSym.
+        
         #Expected Covereage -> Velvet
         #MinCutoff->Velvet
         #PairLength -> MetaSim->PrimaryConf
